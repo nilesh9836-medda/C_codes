d3380768ddd081f6a6baa0882723acf59e3bd603
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-05 13:13:27 +0530

    Add comment_constant.c file

---
+++ codes/basics/comment_constant.c
@@ -0,0 +1,16 @@
+// This a single line comment
+
+/*
+    This a block comment
+*/
+
+#include <stdio.h>
+#define PI 3.1516
+#define OPTION 'A'
+#define Ask "Press Y to continue"
+
+int main(){
+    printf("\nPI=%f and Option:%c\n",PI,OPTION);
+
+    return 0;
+}
