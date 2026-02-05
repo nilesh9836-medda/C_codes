57c95760c49c62e511fa5c9a33d885e3be7cdf2a
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-05 13:12:53 +0530

    Add datatypes.c file

---
+++ codes/basics/datatypes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    short s=3267;
+    int a =123456;
+    int long l=1234567891234;
+    float fl=3.25f;
+    double d = 1;
+    char c ='A';
+    printf("\ns=%d, a=%d, l=%ld, fl=%f, d=%f, and c=%c\n",s,a,l,fl,d,c);
+
+    return 0;
+}
