4d27a02441865f003f6ef35b968c09dbff4848a3
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-08 16:02:18 +0530

    Add 15oddNumList.c file

---
+++ codes/basics/15oddNumList.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int s,e;
+    printf("\nEnter starting point and ending point of the Odd Numbers list: ");
+    scanf("%d%d",&s,&e);
+    if (s<e)
+    {
+        printf("\nThe Odd Numbers List:\n");
+        for(int i=s;i<=e;i++)
+            if (i%2 != 0)
+                printf("\t%d",i);
+    }
+    else
+        printf("\nInvalid Inputs!!!\n");
+
+    return 0;
+}
